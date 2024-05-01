6f18f00531be6d1e556c96f34d2923cc148b2fb1
Needlide <max.litvinyuck553@gmail.com>
2024-05-01 14:46:19 +0300

    Implemented DbContext classes

---
+++ Database/UrlContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URLShortenerAPI.Models;
+
+namespace URLShortenerAPI.Database
+{
+    public class UrlContext : DbContext
+    {
+        public DbSet<UrlEntry> UrlEntries { get; set; }
+    }
+}
---
+++ Database/UserContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URLShortenerAPI.Models;
+
+namespace URLShortenerAPI.Database
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
